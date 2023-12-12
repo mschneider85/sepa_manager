60aa8624b240b8dde356d582af683847a3e241bd
Martin Schneider <m.schneider@cortona.de>
2023-12-12 06:47:00 +0100

    Add admin login system test

---
+++ test/system/admin_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class AdminTest < ApplicationSystemTestCase
+  test "admin logs in" do
+    admin_user = admin_users(:admin_one)
+
+    visit new_admin_user_session_path
+
+    fill_in "Email", with: admin_user.email
+    fill_in "Password", with: "password123"
+
+    click_on "Login"
+
+    assert_current_path admin_root_path
+    assert_text "Dashboard"
+  end
+end
