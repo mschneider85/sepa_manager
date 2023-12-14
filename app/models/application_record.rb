c2efa8d1b5570c575c4a6cd3f52aa94df5136a21
Martin Schneider <m.schneider@cortona.de>
2023-12-14 21:09:27 +0100

    Update ransackable_associations and ransackable_attributes methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  def self.ransackable_associations(_) = reflections.keys
-  def self.ransackable_attributes(_) = attribute_names - %w[token]
+  def self.ransackable_associations(_auth_object = nil) = reflections.keys
+  def self.ransackable_attributes(_auth_object = nil) = attribute_names - %w[token]
 end
