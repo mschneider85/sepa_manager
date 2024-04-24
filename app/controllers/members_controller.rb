81d93e374658698e94e103af4187ef33df78ab7b
Martin Schneider <m.schneider@cortona.de>
2024-04-25 01:03:48 +0200

    Fix bug in MembersController confirm action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,9 @@
 class MembersController < ApplicationController
   def confirm
     member = Member.find_by_token_for!(:confirmation, params[:token])
-    return head(:not_found) unless member
-
+  rescue ActiveSupport::MessageVerifier::InvalidSignature => _e
+    @invalid = true
+  else
     member.confirm!
   end
 end
