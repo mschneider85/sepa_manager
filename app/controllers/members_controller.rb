bfb2aed75cda80199a2c74b9ab050642d36e7077
Martin Schneider <m.schneider@cortona.de>
2024-04-25 13:18:46 +0200

    Add browser gem for bot detection
    Try to solve issue /w BingPreview prefetching the confirmation url

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,7 @@
 class MembersController < ApplicationController
   def confirm
+    return head :forbidden if browser.bot?
+
     member = Member.find_by_token_for!(:confirmation, params[:token])
   rescue ActiveSupport::MessageVerifier::InvalidSignature => _e
     @invalid = true
